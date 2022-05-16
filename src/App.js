cbc6211ab5850199243afd97bcbf20a06d496765
donteghen <donaldteghen@gmail.com>
2022-05-16 13:13:08 +0100

    initial setup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src='https://res.cloudinary.com/dpyl8tyll/image/upload/v1632640244/DenizPlastikAvatars/Deniz%20Azlan.jpg' className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
